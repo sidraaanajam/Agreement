9507d8a39c929186145f71e808cf222454c22015
sidraaanajam <sidranajam2190@gmail.com>
2018-04-05 12:18:51 +0500

    add file
    add file

---
+++ board-members.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<!-- Basic Page Needs
+	================================================== -->
+	<?php include('includes/headstyles.php'); ?>
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<!-- Bxslider -->
+	<link rel="stylesheet" href="css/jquery.bxslider.css">
+
+</head>
+	
+<body>
+	<div class="body-inner">
+	<!-- Header start -->
+	<?php include('includes/header.php'); ?>
+
+	<div id="inner-header">
+		<img src="images/banner/banner2.jpg" alt ="" />
+	</div>
+
+	<!-- Subpage title start -->
+	<section id="inner-title">
+	    <div class="container">
+	      <div class="row">
+	        <div class="col-md-12">
+	        	<div class="inner-title-content">
+		        	<h2>BOARD MEMBERS</h2>
+		        	<ul class="breadcrumb">
+			            <li> <a href="index.php">Home </a></li>
+			            <li> Board Members</li>
+		          	</ul>
+	          	</div>
+	        </div>
+
+	      </div>
+	    </div>
+	 </section>
+	<!-- Subpage title end -->
+
+	<div class="gap-40"></div>
+
+	<!-- About page start -->
+	<section id="about-page" class="m-b-30">
+		<div class="container">
+			<!-- row 1 -->
+			<div class="row">
+				<div class="col-md-5 col-md-push-1 wow slideInLeft">
+					<h3 class="page-content-title"><span>President:</span> Ayesha Bengali</h3>
+					<div class="wpb_wrapper">
+						<p>Ayesha Bengali has been a consummate volunteer in the Chicagoland area for the past 25 years. She brings passion, experience and dedication to all of her endeavors. She devotes a majority of her time to education and charity-based non-profit organizations to fulfill her drive to help those in need. She is a mother of three and grandmother of four.
+						<br><br>
+						Ayesha founded the non-profit organization: Women for Education. The aim of the organization was to provide the highest quality education to young children. Aside from founding the organization, Ayesha served as the Secretary. Ayesha has recently stepped down from WE in order to devote her time to The Hunar Foundation. She has spent the last several years working as the Office Manager for Recycle Processes Inc in Naperville IL. At her local mosque in Chicago, Ayesha is involved in various capacities. She serves as a Board Member for the Women’s Committee at Islamic Foundation. Further, Ayesha was an active volunteer at the Avery Coonley School – an institutions focused on gifted education. She served as volunteer for 11 years.
+						<br><br>
+						Mrs. Bengali currently resides in the western suburbs of Chicago with her husband and youngest son. She can often be found enjoying time with her grandchildren and giving nutritional advice to those who seek her counsel.</p>
+					</div>
+				</div>
+				<div class="col-md-5 col-md-push-1  wow slideInRight">
+					<h3 class="page-content-title"><span>Vice President:</span> Shimmi Kidwai</h3>
+					<div class="wpb_wrapper">
+						<p>For the past thirty years, Shimmi has focused a great deal of her time and effort on supporting social causes and raising funds for charities in the USA, Pakistan and the Washington DC Metropolitan area. She has supported and raised funds for a number of Pakistani and American charities such as Edhi Foundation, Layton Rahmatulla Benevolent Trust (LRBT), SOS, The Kidney Center, Fatimid Foundation, The Imran Khan Foundation / Shaukat Khanum Cancer Hospital, CHAL, JANUM, AMAN Foundation, Children’s Hospital, Lombardi Cancer Center and others.
+						<br><br>
+						She is on the board of the non-profit organizations Friends of Pakistan Club, which recently celebrated their 65th anniversary, and the Association of Pakistani Women in America (APWA).
+						<br><br>
+						Prior to the formal establishment of The Citizens Foundation in the US as a 501-C organization, Shimmi Kidwai started TCF in the US and helped in collecting funds. Her dedication to TCF stems from her belief that education, especially education provided to the under privileged children, is the only hope for Pakistan.
+						<br><br>
+						She was on Board of TCF USA and currently serves as an adviser to the Board. She also serves as the President for the TCF Washington DC Chapter, she is also a Trustee / Director of The Hunar Foundation, USA.
+						</p>
+					</div>				
+				</div>
+			</div>
+			<br><br>
+
+
+			<!-- row 2 -->
+			<div class="row">
+				<div class="col-md-5 col-md-push-1 wow slideInLeft">
+					<h3 class="page-content-title"><span>Director:</span>  Aslam Khaliq</h3>
+					<div class="wpb_wrapper">
+						<p>Aslam Khaliq is presently serving as Chairman Reckitt Benckiser (Pakistan) Ltd. He was former Chairman and adviser of Pakistan Tobacco Company Ltd, a subsidiary of the British American Tobacco Company, which employees over a 100,000 people, with operations in around 180 countries and is a market leader in more than 50 countries selling over 300 brands.
+						<br><br>
+						He is also presently Chairman of The Hunar Foundation & CHAL Foundation (both Not for Profit NGO’s), as well as being on the Board of Governors of Lahore University of Management Sciences (LUMS), Pakistan International Airline (PIA) & PIA Investments.
+						<br><br>
+						He has served on the Boards of Oil & Gas Development Company Ltd, National Data and Registration Authority, Govt. of Pakistan (GOP), Islamabad Stock Exchange, Pakistan Atomic Energy Commission Bio-Sciences (Pvt.) Ltd.
+						<br><br>
+						He has also served as member of the Board of Governors of Indus Valley School of Art & Architecture, Karachi, National Vocational and Training Commission, GOP, and Intellectual Property Organization, GOP.</p>
+					</div>
+				</div>
+				<div class="col-md-5 col-md-push-1  wow slideInLeft">
+					<h3 class="page-content-title"><span>Treasurer:</span> Mehnaz Mulla</h3>
+					<div class="wpb_wrapper">
+						<p> 
+						Mehnaz comes from a strong textile background where she worked for 18 years. She started her own local clothing store in Karachi and then went on to work for American and Pakistani textile buying-houses. Other notable accomplishments:
+						<br><br>
+						– VP Business Development & Customer Relations: Synergies Worldwide<br>
+						– Country Manager: American Pacific Enterprises. An American buying-house for textiles<br>
+						– Textile Merchandiser : Linmark International<br>
+						– Merchandiser: Apparel Buying Services.<br>
+						<br><br>
+						She also worked for Diners Club Pakistan as Corporate Sales Manager. Mehnaz joined the Association of Pakistani Americans in 2010 as a Board member where she also served as Secretary. She moved to Orlando, Florida from Karachi, Pakistan about 6 years ago.
+						</p>
+					</div>				
+				</div>
+			</div>
+			<br><br>
+
+
+			<!-- row 3 -->
+			<div class="row">
+				<div class="col-md-5 col-md-push-1 wow slideInRight">
+					<h3 class="page-content-title"><span>Secretary:</span> Fatima Khan</h3>
+					<div class="wpb_wrapper">
+						<p>Fatima Sultan is a Retail Design Planner with Management and Marketing degrees.
+						Fatima is the Executive Director and co-founder of Zanbeel Art Inc. a non-profit art educational organization, which aims to build bridges between South Asians and Americans via art educational programs. Utilizing art as a tool, the program helps to reduce racism and diffuses post 9/11 fear and racism against South Asian youth.
+						<br><br>
+						Fatima also launched South Asian Art and Literacy for Youth, SAALY– providing art programs in low socio economic (SES) public schools in Los Angeles, where art programs were affected by the statewide budget cuts. SAALY empowers students, transforming them from neighborhood kids to global citizens.
+						Fatima resides in Pacific Palisades California with her husband.</p>
+					</div>
+				</div>
+				<div class="col-md-5 col-md-push-1  wow slideInRight">
+					<h3 class="page-content-title"><span>Assistant Treasurer: </span> Farah Noor Cheema</h3>
+					<div class="wpb_wrapper">
+						<p>Farah is a graduate from King Edward Medical University in Lahore, Pakistan.
+						<br>
+						She has spent the past several years managing her husband’s busy medical practice. A devoted mother of four, she has been actively involved in the home and school associations of various prestigious institutions. Farah’s extensive knowledge in different approaches to education has aided her as a mentor to countless individuals in the community. Her interests include traveling, spending time with friends and family, and staying active. Farah’s experiences living in Pakistan, the United Kingdom, and the United States lend her a dynamic perspective on her charitable work.
+						</p>
+					</div>				
+				</div>
+			</div>
+			<br><br>
+
+
+			<!-- row 4  -->
+			<div class="row">
+				<div class="col-md-10 col-md-push-1 wow slideInLeft">
+					<h3 class="page-content-title"><span>Director:</span> Sehr Karim-Jaffer</h3>
+					<div class="wpb_wrapper">
+						<p> 
+						Sehr spent the last ten years living in New York, where she was a Museum Educator and Program Developer at the Metropolitan Museum of Art. She specialized in Islamic Art and Ancient Near East Art but enjoyed teaching across the entire collection. Sehr’s passion lies in engaging people with art and bridging the gap between art and life.
+						<br><br>
+						Sehr has a Masters of Science in Education with a specialization in Experiential Education from Bank Street School of Education. She studied Art History at Columbia University and Sotheby’s Institute of Art and completed her undergraduate studies at Mount Holyoke College.
+						<br><br>
+						Sehr has been passionate about giving back to Pakistan since she was a little girl. While in New York she was on the committee of LRBT, she worked closely with Developments in Literacy (DiL) and more recently serves as Education Board of Trustee for the Dream Foundation Trust. Sehr credits her father for instilling in her a love for Pakistan and a value for giving back.
+						<br><br>
+						Sehr grew up in the Middle East and recently moved to Karachi with her husband and her one-year-old son, to be closer to family.</p>
+					</div>
+				</div>
+			
+			</div>
+			<br><br>
+
+
+		</div><!--/ container end -->
+	</section><!-- About page end -->
+
+	<!-- Main Footer start -->
+	<?php include('includes/footer.php'); ?>
+	
+
+
+</div>
+
+	<!-- Javascript Files
+	================================================== -->
+	<?php include('includes/scripts.php') ?>
+
+	
+</body>
+</html>
